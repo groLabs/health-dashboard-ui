22caa14a42d7f7a1176ea90e14c8e62afd5bc7fc
Sergi Juanati <wint3rmute@gmail.com>
2021-07-21 12:20:10 +0200

    API call to BE to retrieve gro stats

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import Header from "./Header";
 import Tvl from "./Tvl";
 import Apy from "./Apy";
@@ -10,7 +11,24 @@ import ExposureStables from "./ExposureStables";
 import ExposureProtocols from "./ExposureProtocols";
 
 
+
 const Dashboard = () => {
+    const URL = 'http://localhost:3001/database/gro_stats';
+
+    React.useEffect(() => {
+        fetchGroStats();
+    }, []);
+
+    const fetchGroStats = async () => {
+        await axios.get(URL, {
+            params: { network: 'ropsten' },
+        }).then(res => {
+            console.log('data!', res.data);
+        }).catch(err => {
+            console.log('Error in Dashboard.tsx -> fetchGroStats(): ', err);
+        });
+    };
+
     return (
         <div>
             <Header />
--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import styles from './Dashboard.module.css';
+import styles from './Header.module.css';
 
 // TODO: fixed header, incl. refresh button
 const Header = () => {
     return (
         <div className={styles.container}>
-            Testing header
+            <div className={styles.header}> Health Monitoring Dashboard MVP </div>
+            <div className={styles.container_horizontal}>
+                <div className={styles.subheader}> Last Date: </div>
+                <div className={styles.subheader}> Last Timestamp: </div>
+                <div className={styles.subheader}> Network: </div>
+                <div className={styles.subheader}> Refresh </div>
+            </div>
         </div>
     )
-
 }
 
 export default Header;
