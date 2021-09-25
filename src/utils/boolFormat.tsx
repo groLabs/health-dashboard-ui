7a9d58a9019990843f7833a0de213374c08f4bb7
Sergi Juanati <wint3rmute@gmail.com>
2021-09-25 11:06:45 +0200

    Fixed price check boolean at detail level

--- src/utils/boolFormat.tsx
+++ src/utils/boolFormat.tsx
@@ -1,13 +1,19 @@
 import styles from '../components/dashboard/Dashboard.module.css';
 
+// const boolFormat = (value: boolean) => {
+//     if (value) {
+//         return (value)
+//         ? <span className={styles.green}>true</span>
+//         : <span className={styles.red}>false</span>;
+//     } else {
+//         return '';
+//     }
+// }
+
 const boolFormat = (value: boolean) => {
-    if (value) {
-        return (value)
+    return (value)
         ? <span className={styles.green}>true</span>
         : <span className={styles.red}>false</span>;
-    } else {
-        return '';
-    }
 }
 
 export default boolFormat;
